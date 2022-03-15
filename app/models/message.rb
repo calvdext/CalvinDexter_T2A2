c4984b9a06c7e1ab385d6eb865f35628bc3f7d60
calvdext <gcab022117@coderacademy.edu.au>
2022-03-15 11:28:36 +1000

    adding relations between Messages and Products

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+  has_one :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   belongs_to :subcategory
   has_one_attached :image
   has_many :receiver_messages
-  
+  has_many :messages
 end
